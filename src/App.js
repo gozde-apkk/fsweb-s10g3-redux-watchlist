6ccdc3e14e4fa93e4b70d16f0d04ad30d30d65fa
gozde-apk <gozde.apak@hotmail.com>
2023-06-04 16:52:39 +0300

    AppPage fixed

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import { useState } from "react";
 import { Switch, Route, NavLink } from "react-router-dom";
 import Movie from "./components/Movie";
 import FavMovie from "./components/FavMovie";
+import { useDispatch, useSelector, } from "react-redux";
+import { backToFirst , deleteMovie , previousMovie, nextMovie } from "./store/actions/movieActions";
 
 function App() {
-  const [sira, setSira] = useState(0);
-  const favMovies = [];
+  
+  const favMovies = useSelector((store) => store.FavoriteReducer.favorites);
+  const movies = useSelector((store) => store.movieReducers.movies);
+  const siraNo = useSelector((store) => store.movieReducers.siraNo);
+
+  const dispatch = useDispatch();
+
+  const addFavListHandler = () => {
+    dispatch(deleteMovie(movies[siraNo]));
+    dispatch(addFavorite(movies[siraNo]))
+  }
+
 
   function sonrakiFilm() {
     setSira(sira + 1);
@@ -23,21 +35,43 @@ function App() {
       </nav>
       <Switch>
         <Route exact path="/">
-          <Movie sira={sira} />
+          {movies.length != 0 ?
+            <>
+              <Movie />
 
-          <div className="flex gap-3 justify-end py-3">
-            <button
-              onClick={sonrakiFilm}
-              className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
-            >
-              Sıradaki
-            </button>
-            <button className="select-none px-4 py-2 bg-blue-700 hover:bg-blue-600 text-white">
-              Listeme ekle
-            </button>
-          </div>
+              <div className="flex gap-3 justify-end py-3">
+
+                <button
+                  onClick={() => dispatch(backToFirst())}
+                  className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
+                >
+                  Başa Dön
+                </button>
+
+                {siraNo !== 0 && <button
+                  onClick={() => dispatch(previousMovie(1))}
+                  className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
+                >
+                  Önceki
+                </button>}
+
+                {siraNo !== (movies.length - 1) && <button
+                  onClick={() => dispatch(nextMovie(1))}
+                  className="select-none px-4 py-2 border border-blue-700 text-blue-700 hover:border-blue-500 hover:text-blue-500"
+                >
+                  Sıradaki
+                </button>}
+
+                <button onClick={addFavListHandler} className="select-none px-4 py-2 bg-blue-700 hover:bg-blue-600 text-white">
+                  Listeme ekle
+                </button>
+
+              </div>
+            </>
+            : <h1>Filmler Bitti.....</h1>
+          }
+
         </Route>
-
         <Route path="/listem">
           <div>
             {favMovies.map((movie) => (
