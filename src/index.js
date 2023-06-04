07d4067d803c30c0908040ca2a1b2ea9eac9f3bf
gozde-apk <gozde.apak@hotmail.com>
2023-06-04 16:49:38 +0300

    packagelson

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import App from './App';
 import { BrowserRouter } from "react-router-dom"
+import './index.css';
+
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './store/reducers';
+import { logger } from 'redux-logger';
+ 
+
+const store = createStore(reducer,applyMiddleware(logger));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+    <Provider store = {store}>
     <App />
+    </Provider>
   </BrowserRouter>
 );
 
