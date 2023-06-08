15a2de129a8574f7d5b3c03e30a3ba0e0f3d5c84
gozde-apk <gozde.apak@hotmail.com>
2023-06-08 13:14:47 +0300

    Movie fixed

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,34 +1,44 @@
 import { useSelector } from "react-redux";
+import { movies } from "../movies.js";
 
 
 export default function Movie(props) {
-  const movies = useSelector(store => store.movieReducers.movies)
-  const siraNo = useSelector(store => store.movieReducers.siraNo)
-  const anlikGosterim = movies[siraNo]
+ 
+  const movies = useSelector((store) => store.moviesReducers.movies);
+  const movie = movies[props.sira];
 
 
   return (
+    <div>
+      {movie && (  
     <div className="flex bg-white shadow-lg items-start">
-      <img src={anlikGosterim.posterUrl} alt={anlikGosterim.title} className="max-w-[18rem] w-2/5 block" />
+   
+      <img src={movie.posterUrl}
+       alt={movie.title} className="max-w-[18rem] w-2/5 block"
+        />
       <div className="p-8 flex flex-col gap-4 text-sm">
         <div>
-          <h2 className="text-2xl">{anlikGosterim.title}</h2>
-          <p className="italic text-sm">{anlikGosterim.genres.join(', ')}</p>
+          <h2 className="text-2xl">{movie.title}</h2>
+          <p className="italic text-sm">
+            {Array.isArray(movie.genres) ? movie.genres.join(', '): ""}
+            </p>
         </div>
-        <p className="">{anlikGosterim.plot}</p>
+        <p className="">{movie.plot}</p>
         <div className="flex flex-col sm:flex-row">
           <span className="w-1/3 font-bold">Yönetmen</span>
-          <span className="flex-1">{anlikGosterim.director}</span>
+          <span className="flex-1">{movie.director}</span>
         </div>
         <div className="flex flex-col sm:flex-row">
           <span className="w-1/3 font-bold">Oyuncular</span>
-          <span className="flex-1">{anlikGosterim.actors}</span>
+          <span className="flex-1">{movie.actors}</span>
         </div>
         <div className="flex text-sm gap-1 justify-end">
-          <span className="block px-2 py-1 rounded-md border border-zinc-400">{anlikGosterim.year}</span>
-          <span className="block px-2 py-1 rounded-md border border-zinc-400">{anlikGosterim.runtime}dk</span>
+          <span className="block px-2 py-1 rounded-md border border-zinc-400">{movie.year}</span>
+          <span className="block px-2 py-1 rounded-md border border-zinc-400">{movie.runtime}dk</span>
         </div>
       </div>
     </div>
-  )
+      )}
+  </div>
+  );
 };
